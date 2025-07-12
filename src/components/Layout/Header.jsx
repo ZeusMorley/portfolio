43d76cec16904196090c6156413a953397461698
Zeus Morley S Pineda <zeusmorleypz@gmail.com>
2025-07-12 19:31:23 +0800

    First commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import './Header.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {FiChevronUp} from 'react-icons/fi';
+import NavLinks from './NavLinks';
+
+
+const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  }
+  
+  return (
+    <>
+    <header className="header">
+      <div className="container">
+        <h1 className="header__title">
+          <span className="header__title--first">Zeus</span>
+          <span className="header__title--second">Morley</span>
+        </h1>
+        <nav className="nav-desktop">
+          <NavLinks />
+        </nav>
+
+        {isDropdownOpen && (
+          <div className="dropdown">
+            <NavLinks /> 
+            <button className="dropdown__close" onClick={toggleDropdown}>
+              <FiChevronUp size={28} />
+            </button>
+          </div>
+        )}
+
+        <label htmlFor="dropdown-toggle" className="header__hamburger">
+        <GiHamburgerMenu 
+          className='header__hamburger' 
+          onClick={toggleDropdown}
+        />
+        </label>
+      </div>
+    </header>
+      
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/NavLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavLinks.css';
+
+const NavLinks = () => {
+  return (
+    <ul className="nav-links">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  );
+};
+
+export default NavLinks;
